546c70c97384e35774169f3f1956bc045d028e77
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-08-12 14:22:26 -0300

    :sparkles: feat: Client post route

--- src/routes/client.router.ts
+++ src/routes/client.router.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express'
-// import ClientController from '../app/controller/ClientController'
-// import createValidation from '../app/validations/client/create'
+import ClientCreateValidate from '../app/validations/client/ClientCreateValidate'
+import ClientController from '../app/controllers/ClientController'
 // import updateValidation from '../app/validations/client/update'
 
 const routes = Router()
 
-// routes.post('api/v1/client', createValidation, ClientController.post)
-// routes.get('/api/v1/client', ClientController.get)
+routes.post('/api/v1/client', ClientCreateValidate, ClientController.create)
 // routes.put('/api/v1/client/:id', updateValidation, ClientController.put)
 // routes.delete('/api/v1/client/:id', ClientController.delete)
 
