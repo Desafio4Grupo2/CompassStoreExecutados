2270068b3dcca249d271475de63622dc3cedf234
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-08-12 18:44:34 -0300

    :recycle: refactor: Response error handling

--- src/app/services/ClientService.ts
+++ src/app/services/ClientService.ts
@@ -2,6 +2,7 @@ import { PaginateResult } from 'mongoose'
 import { IClient, IClientResponse } from '../interfaces/IClient'
 import ClientRepository from '../repositories/ClientRepository'
 import bcrypt from 'bcryptjs'
+import BadRequestError from '../errors/BadRequestError'
 
 class ClientService {
   public async get (payload: any, page: any): Promise<PaginateResult<IClient>> { // any
@@ -21,21 +22,18 @@ class ClientService {
   }
 
   async create (payload: IClient): Promise<IClientResponse> {
-    try {
-      // hashing the password
-      const salt = await bcrypt.genSalt(10)
-      payload.password = await bcrypt.hash(payload.password, salt)
+    const salt = await bcrypt.genSalt(10)
+    payload.password = await bcrypt.hash(payload.password, salt)
 
-      // creating the client
-      const result = await ClientRepository.create(payload)
+    const result = await ClientRepository.create(payload)
 
-      // formatting cpf
-      result.cpf = result.cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4')
-      return result
-    } catch (error) {
-      console.log(error)
-      throw new Error()
+    if (result) {
+      throw new BadRequestError('Client not created')
     }
+
+    result.cpf = result.cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4')
+
+    return result
   }
 }
 
