6853d46dd9e1dc314324d8fc0fe84da8078ca623
Antonio Ribeiro <antonioribeiroamg@gmail.com>
2022-08-12 12:18:06 -0400

    :sparkles: feat: Create product get

--- src/app/controllers/ClientController.ts
+++ src/app/controllers/ClientController.ts
@@ -5,9 +5,9 @@ import ClientService from '../services/ClientService'
 class ClientController {
   public async get (req: Request, res: Response): Promise<Response> {
     try {
-      const body = req.query
+      const { page, ...body } = req.query
 
-      const result = await ClientService.get(body)
+      const result = await ClientService.get(body, page)
       return res.status(201).json(result)
     } catch (error) {
       return res.status(400).json({ error })
