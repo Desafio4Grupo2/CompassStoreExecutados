06e0163ea1334aeed0d403e642ab8e6cc31e2126
Antonio Ribeiro <antonioribeiroamg@gmail.com>
2022-08-12 17:26:50 -0400

    :sparkles: feat: Create clientGetValidate

--- src/routes/client.router.ts
+++ src/routes/client.router.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express'
-import ClientCreateValidate from '../app/validations/client/ClientCreateValidate'
 import ClientController from '../app/controllers/ClientController'
-import updateValidation from '../app/validations/client/UpdateCreateValidate'
+import ClientCreateValidate from '../app/validations/client/ClientCreateValidate'
+import ClientUpdateValidate from '../app/validations/client/ClientUpdateValidate'
+import ClientGetValidate from '../app/validations/client/ClientGetValidate'
 
 const routes = Router()
 
-routes.get('/api/v1/client', ClientController.get)
+routes.get('/api/v1/client', ClientGetValidate, ClientController.get)
 routes.post('/api/v1/client', ClientCreateValidate, ClientController.create)
-routes.put('/api/v1/client/:id', updateValidation, ClientController.updateClient)
+routes.put('/api/v1/client/:id', ClientUpdateValidate, ClientController.updateClient)
 // routes.delete('/api/v1/client/:id', ClientController.delete)
 
 export default routes
