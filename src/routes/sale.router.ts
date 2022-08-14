061184c3da6ac1070583bd140668f2e558ebcd75
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-08-14 19:58:43 -0300

    :sparkles: feat: Add middleware validator in sale create

--- src/routes/sale.router.ts
+++ src/routes/sale.router.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express'
 import SaleController from '../app/controllers/SaleController'
-// import createValidation from '../app/validations/sale/create'
+import SaleCreateValidation from '../app/validations/sale/SaleCreateValidate'
 import updateValidation from '../app/validations/sale/SaleUpdateValidate'
 
 const routes = Router()
 
-routes.post('/api/v1/sale', SaleController.createSale)
+routes.post('/api/v1/sale', SaleCreateValidation, SaleController.createSale)
 routes.get('/api/v1/sale', SaleController.get)
 routes.get('/api/v1/sale/:id', SaleController.getById)
 routes.put('/api/v1/sale/:id', updateValidation, SaleController.update)
