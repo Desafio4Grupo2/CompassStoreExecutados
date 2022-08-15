7ad801e46ed5ea8deaa593964c4eb7d57846faf1
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-08-14 23:47:55 -0300

    :bricks: ci: Criação do  Dockerfile

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import App from './app'
 
 const app = new App()
 
-app.init().listen(process.env.PORT || 3000, () => {
-  console.info(`App starting at http://localhost:${process.env.PORT || 3000}`)
+app.init().listen(process.env.API_EXPOSED_PORT || process.env.PORT || 3000, () => {
+  console.info(`App starting at http://localhost:${process.env.API_EXPOSED_PORT || process.env.PORT || 3000}`)
   console.info(`Envs: ${process.env.TARGET || 'local'}`)
 })
